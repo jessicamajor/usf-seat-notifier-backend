1374695158860ab31c10a99ac2c654ad554a173c
Jessica <jessicamajor07@gmail.com>
2025-11-22 16:37:55 -0500

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
-
-class Subscription(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    phone: str
-    crn: str
-    last_seats: int = 0
